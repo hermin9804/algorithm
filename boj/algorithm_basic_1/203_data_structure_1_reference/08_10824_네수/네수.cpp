4ddc312241b3cb44318d96e64235d17c8cd07e85
hermin9804 <hermin9804@gmail.com>
2021-11-15 12:23:13 +0900

    boj_10824

---
+++ boj/algorithm_basic_1/203_data_structure_1_reference/08_10824_네수/네수.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int	main(void)
+{
+	string a, b, c, d;
+	cin >> a >> b >> c >> d;
+
+	a += b;
+	c += d;
+
+	cout << stoll(a) + stoll(c) << endl;
+	return (0);
+}
