89754ac866a538675b30ddab72889206cf091139
hermin9804 <hermin9804@gmail.com>
2022-01-17 16:03:27 +0900

    [feat] bakingdog 05_Stack workbook

--- bakingdog/05_stack/boj_6198_옥상정원꾸미기.cpp
+++ bakingdog/05_stack/boj_6198_옥상정원꾸미기.cpp
@@ -1,33 +1,24 @@
-#include <stack>
+#include <bits/stdc++.h>
 using namespace std;
 
-typedef struct s_tower {
-	int height;
-	int max;
-} t_tower;
-
-int N, result;
-stack<t_tower> tower;
+stack<int> s;
+int n;
+long long ans;
 
 int main() {
-  ios_base::sync_with_stdio(0);
-  cin.tie(0);
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
 
-  t_tower first;
-  first.height = 0;
-  first.max = 0;
-  tower.push(first);
-
-  cin >> N; 
-  for (int i = 1; i <= N; i++) {
-    t_tower cur;
-    cin >> cur.height;
-    cur.max = tower.top.max;
-	if (cur.max > cur.hight)
-		++result;
-	else
-		cur.max = cur.hight;
-    tower.push(cur);      
-  }
-  cout << result;
+	cin >> n;
+	long long h;
+	while (n--) {
+		cin >> h;
+		while (!s.empty() && s.top() <= h) {
+			s.pop();
+		}
+		ans += s.size();
+		s.push(h);
+	}
+	cout << ans;
+	return 0;
 }
